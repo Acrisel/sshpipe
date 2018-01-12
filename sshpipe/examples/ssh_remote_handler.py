f1c827e23c2a850ce11b2e58e7f87406b126d01f
Arnon Sela <arnon@acrisel.com>
2018-01-12 14:47:15 -0600

    add paremeter to example

--- sshpipe/examples/ssh_remote_handler.py
+++ sshpipe/examples/ssh_remote_handler.py
@@ -16,14 +16,13 @@ class MySSHPipeHandler(SSHPipeHandler):
 
     def __init__(self, count, *args, **kwargs):
         super(MySSHPipeHandler, self).__init__(*args, **kwargs)
-        print('Super initiated', type(super(MySSHPipeHandler, self)).__name__, list(super(MySSHPipeHandler, self).__dict__.keys()))
         self.count = count
         self.file = None
 
     def atstart(self, received):
         file = "{}{}".format(__file__, ".remote.log")
-        if not hasattr(super(MySSHPipeHandler, self), 'mlogger'):
-            raise RuntimeError("Super missing mlogger method.")
+        # if not hasattr(super(MySSHPipeHandler, self), 'mlogger'):
+        #     raise RuntimeError("Super missing mlogger method.")
         if not hasattr(self, 'mlogger'):
             raise RuntimeError("Self missing mlogger method.")
         self.mlogger.debug("Opening file: {}.".format(file))
