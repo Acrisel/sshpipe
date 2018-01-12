c0f90aacaf1301e67cdc5c24e62ecde4d7f6f7e7
Arnon Sela <arnon@acrisel.com>
2018-01-12 14:11:57 -0600

    add paremeter to example

--- sshpipe/examples/ssh_remote_handler.py
+++ sshpipe/examples/ssh_remote_handler.py
@@ -16,6 +16,7 @@ class MySSHPipeHandler(SSHPipeHandler):
 
     def __init__(self, count, *args, **kwargs):
         super(MySSHPipeHandler, self).__init__(*args, **kwargs)
+        print('Super initiated')
         self.count = count
         self.file = None
 
