636aa34a0820a18a9d74c0dfb43696f9406b0fab
Arnon Sela <arnon@acrisel.com>
2018-01-13 21:44:08 -0600

    debug

--- sshpipe/examples/ssh_callback_handler.py
+++ sshpipe/examples/ssh_callback_handler.py
@@ -19,7 +19,7 @@ class MySSHPipeHandler(SSHPipeHandler):
         super(MySSHPipeHandler, self).__init__(*args, **kwargs)
         self.file = None
         here = os.path.dirname(__file__)
-        bindir = os.path.join(os.path.dirname(here), 'bin')
+        bindir = os.path.join(os.path.dirname(here), 'sshpipe', 'bin')
         handler = os.path.join(bindir, 'sshpipe_socket_handler.py')
         self.mlogger.debug("Initiating callback tunnel: {}.".format(handler))
         self.tunnel = SSHTunnel(host, [handler, '--id', type(self).__name__, '--port', str(port),], logger=self.mlogger)
